b165745925845411a84cfbade9e4325833979b2d
Alexander <webmasterdracarys@gmail.com>
2024-12-19 23:41:05 +0300

    main: на Шиву добавлено восточное ЗП

--- backend/maps_data.py
+++ backend/maps_data.py
@@ -28,7 +28,10 @@ class MapsData:
             'north': {'coords': (242, 125), 'direction': 'Север', 'label': 'Правый угол взлетки ЗП-1'},
             'south': {'coords': (167, 42), 'direction': 'Юг', 'label': 'Правый угол взлетки ЗП-2'},
         },
-        'shivas': {'north': {'coords': (30, 150), 'direction': 'Север', 'label': 'Правый угол взлетки ЗП-2'}},
+        'shivas': {
+            'north': {'coords': (30, 150), 'direction': 'Север', 'label': 'Правый угол взлетки ЗП-2'},
+            'south': {'coords': (30, 42), 'direction': 'Юг', 'label': 'Справа дерево у взлетки ЗП-1'},
+        },
         'trijent': {
             'north': {'coords': (40, 215), 'direction': 'Север', 'label': 'Правый угол взлетки ЗП-1'},
             'south': {'coords': (27, 35), 'direction': 'Юг', 'label': 'Правый угол взлетки ЗП-2'},
